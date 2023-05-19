fb3d31df68a31ff7b5ee0feb1586a40248987c29
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2023-05-19 10:05:32 -0400

    add cors , helmet and compression middlewares

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+var cors = require('cors')
+var compression = require('express-compression')
+const helmet = require('helmet');
+
 const { ConnectToMongodb } = require('./database/databaseInit');
 const todoRouter = require('./route/tod.route');
 const { errorLogger , errorResponder, invalidPathHandler } = require('./middleware/errorHandling');
 
 const port = 3000; // You can change this to your desired port number
 
+
+app.use(cors())
+app.use(compression())
+app.use(helmet());
 
 ConnectToMongodb() // connect to mongodb
 app.get('/health', (req, res) => {
